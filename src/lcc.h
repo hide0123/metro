44edfecf4da7c1a31d1a35d23f6bc92742d28f8e
bomkei <93323570+bomkei@users.noreply.github.com>
2023-02-01 02:26:32 +0900

    wip

--- src/lcc.h
+++ src/lcc.h
@@ -2,6 +2,7 @@
 
 #include <string>
 #include <vector>
+#include <list>
 
 enum TokenKind {
   TOK_Int,
@@ -9,9 +10,57 @@ enum TokenKind {
   TOK_Char,
   TOK_String,
   TOK_Ident,
-  TOK_
+  TOK_Punctuater,
+  TOK_End
 };
 
 struct Token {
+  TokenKind kind;
+  
+  std::string_view str;
+  // Token* prev;
+  // Token* next;
+
+  Token(TokenKind kind)
+    : kind(TOK_Int)
+  {
+  }
+};
+
+enum ASTKind {
+  AST_Value,
+  AST_Variable,
+
+  AST_Add,
+  AST_Sub,
+
+  AST_Function
+};
+
+namespace AST {
+
+struct Base {
+  ASTKind kind;
+  Token const& token;
+};
+
+}
+
+class Lexer {
+public:
+  Lexer(std::string const& source);
+  ~Lexer();
+
+  std::list<Token> lex();
+
+
+private:
+
+  std::string const& source;
+  size_t position;
+};
+
+class Parser {
 
 };
+
