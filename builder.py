6c7d52985171fb34cf28586bb241bc033d1b1057
bomkei <93323570+bomkei@users.noreply.github.com>
2023-02-16 10:11:35 +0900

    add builder.py

---
+++ builder.py
@@ -0,0 +1,21 @@
+import os
+import sys
+import glob
+
+class Builder:
+
+  def __init__(self):
+    self.target = ""
+
+  
+
+
+
+def main(args: list) -> int:
+  
+
+  return 0
+
+
+main(sys.argv)
+
